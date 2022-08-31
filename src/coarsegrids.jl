f5fe37118f8efedb20b1be73796a017780666cff
Lucas C Wilcox <lucas@swirlee.com>
2022-08-31 12:04:53 -0700

    Add basic coarse grids and grid manager

---
+++ src/coarsegrids.jl
@@ -0,0 +1,195 @@
+abstract type AbstractCoarseGrid end
+
+struct BrickGrid{T,C,D,M} <: AbstractCoarseGrid
+    connectivity::C
+    coordinates::D
+    mapping::M
+end
+
+connectivity(g::BrickGrid) = g.connectivity
+coordinates(g::BrickGrid) = g.coordinates
+mapping(g::BrickGrid) = g.mapping
+function vertices(g::BrickGrid{T}) where {T}
+    conn = connectivity(g)
+    coords = coordinates(g)
+    m = mapping(g)
+    indices =
+        GC.@preserve conn convert.(Tuple{Int,Int,Int}, P4estTypes.unsafe_vertices(conn))
+    if conn isa P4estTypes.Connectivity{4}
+        verts = [SVector(coords[1][i[1]+1], coords[2][i[2]+1]) for i in indices]
+    else
+        verts = [
+            SVector(coords[1][i[1]+1], coords[2][i[2]+1], coords[3][i[3]+1]) for
+            i in indices
+        ]
+    end
+    return m.(verts)
+end
+function cells(g::BrickGrid)
+    conn = connectivity(g)
+    GC.@preserve conn map.(x -> x + 1, P4estTypes.unsafe_trees(conn))
+end
+
+function brick(
+    T::Type,
+    n::Tuple{Integer,Integer},
+    p::Tuple{Bool,Bool} = (false, false);
+    coordinates = (zero(T):n[1], zero(T):n[2]),
+    mapping = identity,
+)
+
+    connectivity = P4estTypes.brick(n, p)
+
+    return BrickGrid{T,typeof(connectivity),typeof(coordinates),typeof(mapping)}(
+        connectivity,
+        coordinates,
+        mapping,
+    )
+end
+
+function brick(
+    T::Type,
+    n::Tuple{Integer,Integer,Integer},
+    p::Tuple{Bool,Bool,Bool} = (false, false, false);
+    coordinates = (zero(T):n[1], zero(T):n[2], zero(T):n[3]),
+    mapping = identity,
+)
+
+    connectivity = P4estTypes.brick(n, p)
+
+    return BrickGrid{T,typeof(connectivity),typeof(coordinates),typeof(mapping)}(
+        connectivity,
+        coordinates,
+        mapping,
+    )
+end
+
+function brick(n::Tuple{Integer,Integer}, p::Tuple{Bool,Bool} = (false, false); kwargs...)
+    return brick(Float64, n, p, kwargs...)
+end
+
+function brick(
+    n::Tuple{Integer,Integer,Integer},
+    p::Tuple{Bool,Bool,Bool} = (false, false, false);
+    kwargs...,
+)
+    return brick(Float64, n, p, kwargs...)
+end
+
+brick(l::Integer, m::Integer, p::Bool = false, q::Bool = false; kwargs...) =
+    brick(Float64, (l, m), (p, q); kwargs...)
+brick(T::Type, l::Integer, m::Integer, p::Bool = false, q::Bool = false; kwargs) =
+    brick(T, (l, m), (p, q); kwargs...)
+
+function brick(
+    l::Integer,
+    m::Integer,
+    n::Integer,
+    p::Bool = false,
+    q::Bool = false,
+    r::Bool = false;
+    kwargs...,
+)
+    return brick(Float64, (l, m, n), (p, q, r); kwargs...)
+end
+
+function brick(
+    T::Type,
+    l::Integer,
+    m::Integer,
+    n::Integer,
+    p::Bool = false,
+    q::Bool = false,
+    r::Bool = false;
+    kwargs...,
+)
+    return brick(T, (l, m, n), (p, q, r); kwargs...)
+end
+
+
+@recipe function f(coarsegrid::BrickGrid)
+    cs = cells(coarsegrid)
+    vs = vertices(coarsegrid)
+
+    xlims = extrema(getindex.(vs, 1))
+    ylims = extrema(getindex.(vs, 2))
+    zlims = try
+        extrema(getindex.(vs, 3))
+    catch
+        (zero(eltype(xlims)), zero(eltype(xlims)))
+    end
+    isconstz = zlims[1] == zlims[2]
+
+    xlabel --> "x"
+    ylabel --> "y"
+    zlabel --> "z"
+
+    aspect_ratio --> :equal
+    legend --> false
+    grid --> false
+
+    @series begin
+        seriestype --> :path
+        linecolor --> :gray
+        linewidth --> 1
+
+        x = []
+        y = []
+        z = []
+        if length(first(cs)) == 4
+            for c in cs
+                for i in (1, 2, 4, 3, 1)
+
+                    xs = vs[c[i]]
+
+                    push!(x, xs[1])
+                    push!(y, xs[2])
+                    if !isconstz
+                        push!(z, xs[3])
+                    end
+                end
+
+                push!(x, NaN)
+                push!(y, NaN)
+                if !isconstz
+                    push!(z, NaN)
+                end
+            end
+        elseif length(first(cs)) == 8
+            for c in cs
+                for j in (0, 4)
+                    for i in (1 + j, 2 + j, 4 + j, 3 + j, 1 + j)
+                        xi, yi, zi = vs[c[i]]
+
+                        push!(x, xi)
+                        push!(y, yi)
+                        push!(z, zi)
+                    end
+
+                    push!(x, NaN)
+                    push!(y, NaN)
+                    push!(z, NaN)
+                end
+
+                for j = 0:3
+                    for i in (1 + j, 5 + j)
+                        xi, yi, zi = vs[c[i]]
+
+                        push!(x, xi)
+                        push!(y, yi)
+                        push!(z, zi)
+                    end
+
+                    push!(x, NaN)
+                    push!(y, NaN)
+                    push!(z, NaN)
+                end
+            end
+        end
+        if isconstz
+            x, y
+        else
+            x, y, z
+        end
+    end
+end
---
+++ src/gridmanager.jl
@@ -0,0 +1,101 @@
+abstract type AbstractGridManager end
+
+struct QuadData
+    old_id::P4estTypes.Locidx
+    old_level::Int8
+    adapt_flag::UInt8
+end
+
+const AdaptCoarsen = 0x00
+const AdaptNone = 0x01
+const AdaptRefine = 0x10
+const AdaptTouched = 0x11
+
+struct GridManager{C<:AbstractCell,G<:AbstractCoarseGrid,V,P} <: AbstractGridManager
+    referencecell::C
+    coarsegrid::G
+    coarsegridvertices::V
+    forest::P
+end
+
+function GridManager(
+    referencecell,
+    coarsegrid;
+    comm = MPI.COMM_WORLD,
+    min_level = 0,
+    fill_uniform = true,
+)
+    p = P4estTypes.pxest(
+        connectivity(coarsegrid);
+        min_level = first(min_level),
+        data_type = QuadData,
+        comm,
+        fill_uniform,
+    )
+
+    coarsegridvertices = adapt(arraytype(referencecell), vertices(coarsegrid))
+
+    return GridManager(referencecell, coarsegrid, coarsegridvertices, p)
+end
+
+Base.length(gm::GridManager) = P4estTypes.lengthoflocalquadrants(gm.forest)
+
+function fill_quadrant_user_data(forest, _, quadrant, quadrantid, treeid, flags)
+    id = quadrantid + P4estTypes.offset(forest[treeid])
+    P4estTypes.storeuserdata!(quadrant, QuadData(id, P4estTypes.level(quadrant), flags[id]))
+end
+
+function fill_adapt_flags(::P4estTypes.Pxest{X}, _, quadrant, _, _, flags) where {X}
+    d = P4estTypes.loaduserdata(quadrant)
+
+    if d.old_level < P4estTypes.level(quadrant)
+        flags[d.old_id] = AdaptRefine
+    elseif d.old_level > P4estTypes.level(quadrant)
+        for i = 0:X-1
+            flags[d.old_id+i] = AdaptCoarsen
+        end
+    else
+        flags[d.old_id] = AdaptNone
+    end
+
+    return
+end
+
+function replace_quads(_, _, outgoing, incoming)
+    outd = P4estTypes.loaduserdata(first(outgoing))
+
+    for quadrant in incoming
+        P4estTypes.storeuserdata!(
+            quadrant,
+            QuadData(outd.old_id, outd.old_level, AdaptTouched),
+        )
+    end
+end
+
+function refine_quads(_, _, quadrant)
+    retval = P4estTypes.loaduserdata(quadrant).adapt_flag == AdaptRefine
+    return retval
+end
+
+function coarsen_quads(_, _, children)
+    coarsen = true
+
+    for child in children
+        coarsen &= P4estTypes.loaduserdata(child).adapt_flag == AdaptCoarsen
+    end
+
+    return coarsen
+end
+
+function adapt!(gm::GridManager, flags)
+    @assert length(gm) == length(flags)
+
+    P4estTypes.iterateforest(gm.forest; userdata = flags, volume = fill_quadrant_user_data)
+
+    P4estTypes.coarsen!(gm.forest; coarsen = coarsen_quads, replace = replace_quads)
+    P4estTypes.refine!(gm.forest; refine = refine_quads, replace = replace_quads)
+    P4estTypes.balance!(gm.forest, replace = replace_quads)
+
+    flags .= AdaptNone
+    P4estTypes.iterateforest(gm.forest; userdata = flags, volume = fill_adapt_flags)
+end
