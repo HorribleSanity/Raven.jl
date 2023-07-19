714cfa9c341791ac366d8f8ad7c457ce16161b53
Lucas C Wilcox <lucas@swirlee.com>
2023-07-19 12:22:47 -0700

    Add `size(::AbstractCell, ::Integer)`

--- src/cells.jl
+++ src/cells.jl
@@ -4,6 +4,7 @@ floattype(::Type{<:AbstractCell{S,T}}) where {S,T} = T
 arraytype(::Type{<:AbstractCell{S,T,A}}) where {S,T,A} = A
 Base.ndims(::Type{<:AbstractCell{S,T,A,N}}) where {S,T,A,N} = N
 Base.size(::Type{<:AbstractCell{S,T,A}}) where {S,T,A} = size_to_tuple(S)
+Base.size(::Type{<:AbstractCell{S,T,A}}, i::Integer) where {S,T,A} = size_to_tuple(S)[i]
 Base.length(::Type{<:AbstractCell{S,T,A}}) where {S,T,A} = tuple_prod(S)
 Base.strides(::Type{<:AbstractCell{T,A,S}}) where {S,T,A} =
     Base.size_to_strides(1, size_to_tuple(S)...)
@@ -12,6 +13,7 @@ floattype(cell::AbstractCell) = floattype(typeof(cell))
 arraytype(cell::AbstractCell) = arraytype(typeof(cell))
 Base.ndims(cell::AbstractCell) = Base.ndims(typeof(cell))
 Base.size(cell::AbstractCell) = Base.size(typeof(cell))
+Base.size(cell::AbstractCell, i::Integer) = Base.size(typeof(cell), i)
 Base.length(cell::AbstractCell) = Base.length(typeof(cell))
 Base.strides(cell::AbstractCell) = Base.strides(typeof(cell))
 
